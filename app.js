33f1a07b9da3b1bc3b1dccd9f4c454e91ac4065e
keyur diwan <keyurdiwan009@gmail.com>
2021-12-18 16:56:10 +0530

    work on functionality..!
    add kill enemy feature!
    remove weapons from array!
    work on enemy color!
    add faded animation on weapons and enemies!
    add gsap library!

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+
+// basics environment setup
 const canvas = document.createElement('canvas');
 
 document.querySelector(".myGame").appendChild(canvas);
@@ -12,14 +14,38 @@ let difficulty = 2;
 const form = document.querySelector("form");
 const scoreBoard = document.querySelector(".scoreBoard");
 
+// basics functions
+
+// EventListener for difficulty
 document.querySelector("input").addEventListener("click", (e) => {
     e.preventDefault();
 
+    // making form invisible
     form.style.display = "none";
+    // making scoreBoard visible
     scoreBoard.style.display = "block";
-
+    // getting difficulty selected user
     const userValue = document.getElementById("difficulty").value;
-    alert(userValue);
+    // alert(userValue);
+    if(userValue === 'Easy'){
+        setInterval(spawnEnemy, 2000);
+        return difficulty = 5;
+    }
+
+    if(userValue === 'Medium'){
+        setInterval(spawnEnemy, 1500)
+        return difficulty = 8;
+    }
+
+    if(userValue === 'Hard'){
+        setInterval(spawnEnemy, 1100)
+        return difficulty = 10;
+    }
+
+    if(userValue === 'Insane'){
+        setInterval(spawnEnemy, 800)
+        return difficulty = 12;
+    }
 
 })
 
@@ -128,19 +154,23 @@ class Enemy{
 
 
 
-// ------------------
+// ------------------ Main Code Start Here ---------------------
+
+
 const a = new Player(playerPosition.x, 
     playerPosition.y, 
     15, 
-    `rgb(${Math.random() * 250},${Math.random() * 250},${Math.random() * 250})`);
+   "whitesmoke");
 
 const weapons = []
 const enemies = []
 
+//  function to create spawnEnemy at random location
 const spawnEnemy = () => {
 
+    // generating random size of enemy
     const enemySize = Math.random() * ( 40 - 5 ) + 5;
-    const enemyColor =  `rgb(${Math.random() * 250},${Math.random() * 250},${Math.random() * 250})`
+    const enemyColor =  `hsl(${Math.floor(Math.random() * 360)}, 100%, 50%)`
 
     let random;
 
@@ -164,33 +194,91 @@ const spawnEnemy = () => {
         );
 
     const velocity = {
-        x:Math.cos(myAngle) * 5,
-        y:Math.sin(myAngle) * 5, 
+        x:Math.cos(myAngle) * difficulty,
+        y:Math.sin(myAngle) * difficulty, 
         
     }; 
 
     enemies.push(new Enemy(random.x, random.y, enemySize, enemyColor, velocity))
 }
 
-function animation(){
-    requestAnimationFrame(animation);
 
-    context.clearRect(0, 0, canvas.width, canvas.height);
+// ----------- creating animation function ---------
+
+let animationId
+
+function animation(){
+    animationId = requestAnimationFrame(animation);
+
+    context.fillStyle = "rgba(0,0,0,0.2)"
+    context.fillRect(0, 0, canvas.width, canvas.height);
+    // context.clearRect
     // console.log(Math.random())
     a.draw();
     
-    weapons.forEach((weapon) => {
+    // generating Bullets
+    weapons.forEach((weapon, weaponIndex) => {
         // weapon.draw();
         weapon.update();
+        if(
+            weapon.x + weapon.radius < 1 || 
+            weapon.y + weapon.radius < 1 || 
+            weapon.x - weapon.radius > canvas.width ||
+            weapon.y - weapon.radius > canvas.height
+        )   {
+            weapons.splice(weaponIndex, 1)
+        }
     });
-
-    enemies.forEach((enemy) => {
+    //  generating enemies
+    enemies.forEach((enemy, enemyIndex) => {
+        
+       
+        
         enemy.update();
+        const distanceBetweenPlayerAndEnemy  =  Math.hypot(
+            a.x - enemy.x,
+            a.y - enemy.y)
+
+            if(distanceBetweenPlayerAndEnemy - a.radius - enemy.radius < 1){
+                cancelAnimationFrame(animationId)
+                // console.log("Game Over..!")
+            }
+
+        weapons.forEach((weapon, weaponIndex) =>{
+            const distanceBetweenWeaponAndEnemy = Math.hypot(
+                weapon.x - enemy.x,
+                weapon.y - enemy.y)
+
+                if(distanceBetweenWeaponAndEnemy - weapon.radius - enemy.radius < 1){
+                    // console.log("Killed")
+                   
+                    if(enemy.radius > 15){
+                      gsap.to(enemy, {
+                          radius: enemy.radius - 10,
+                      })
+                        // enemy.radius -= 5;
+                        setTimeout( () => {
+                            // enemies.splice(enemyIndex, 1); 
+                           
+                            weapons.splice(weaponIndex, 1);
+                           }, 0)
+                    } else{
+                        setTimeout( () => {
+                            enemies.splice(enemyIndex, 1); 
+                           
+                            weapons.splice(weaponIndex, 1);
+                           }, 0)
+                    }
+                }
+        })
     });
 }
-
+// console.log(gsap)
 // setInterval(spawnEnemy, 1000)
 
+
+//  ------------- Adding eventListener --------------------------------
+ 
 canvas.addEventListener("click", (e) => {
     
     const myAngle = Math.atan2(
@@ -199,8 +287,8 @@ canvas.addEventListener("click", (e) => {
         );
 
     const velocity = {
-        y:Math.sin(myAngle) * 10, 
-        x:Math.cos(myAngle) * 10
+        y:Math.sin(myAngle) * 6, 
+        x:Math.cos(myAngle) * 6
     };
 //    console.log(myAngle);
 
